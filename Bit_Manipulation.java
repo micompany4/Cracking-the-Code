0259156f14d11506ca9a98c2cb26c5f5c926825e
micompany4 <michael.j.wong@sjsu.edu>
2020-09-02 14:42:07 -0700

    Update Bit_Manipulation.java

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -48,11 +48,112 @@ public class Bit_Manipulation
 	}
 	
 	
+	public static void decimalToBinary(int num)
+	{
+		int quotient = num;
+		int binary[] = new int[64];
+		int count = 0;
+		
+		
+		while(quotient != 0)		//no leading zeroes
+		{
+			binary[count++] = quotient%2;		//set the bit to 0 or 1 depending on it's remainder 
+			quotient /= 2;						
+			
+		}
+		
+		System.out.print("Binary number: ");
+		for(int j = count-1; j >=0; j--)		//the result is inputed backwards, so print it in reverse order
+		{	
+			System.out.print(binary[j]);
+		}
+		
+		
+	}
+	
+	public static int binaryToDecimal(int[] bin)
+	{
+		int decimal = 0;
+		double index = 0;
+		double power = 0;
+		
+		// if it wasn't an array, you would have to do some "fancy" stuff like /10 and %10
+		for(int i = bin.length-1; i >= 0; i--)
+		{
+			//the logic for converting binary to decimal i.e. the important part 
+			index = Math.pow(2, power++);
+			if(bin[i] == 1)
+			{
+				decimal += index;
+			}
+			
+		}
+		
+		return decimal;
+	}
+
+	public static String sumOfBinary(String num1, String num2)
+	{
+		//pads the beginning of the shorter binary number with 0's so that they line up properly 
+		if(num1.length() > num2.length())
+		{
+			while(num2.length() < num1.length())
+			{
+				num2 = "0" + num2;
+			}
+		}
+		if(num2.length() > num1.length())
+		{
+			while(num1.length() < num2.length())
+			{
+				num1 = "0" + num1;
+			}
+		}
+		
+		//formatting purposes to make it look nice
+		System.out.println("  " + num1);
+		System.out.println("+ " + num2);
+		System.out.println("____________");
+		
+		String binary = "";
+		int remainder = 0;		//flag if there is a remainder to add to the calculation 
+		
+		for(int i = num1.length()-1; i >= 0; i--)
+		{
+			int bit1 = Integer.parseInt(num1.substring(i, i+1));
+			int bit2 = Integer.parseInt(num2.substring(i, i+1));
+			
+			int sum = (bit1 + bit2 + remainder) % 2;
+			remainder = (bit1 + bit2 + remainder) / 2;
+			
+			binary = ""+sum + binary;
+			
+		}
+		
+		//at the end of the operation, if there is still a remainder, append it in the first index
+		if(remainder != 0)
+		{
+			binary = ""+ remainder + binary;
+			return " " + binary;		//formatting purposes
+		}
+		else
+		{
+			return "  " + binary;		//formatting purposes
+		}
+		
+		
+	}
+	
 	public static void main(String args[])
 	{
-		int number = 6;
+		int number = 15;
 		int bit = 1;
-		System.out.println(getBit(number, bit));
+		int binaryNum[] = { 1, 1, 0, };
+		
+		String b1 = "101";
+		String b2 = "110";
+		System.out.println(sumOfBinary(b1, b2));
+		
 	}
 	
 }
